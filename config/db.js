9951b7c733085e92428b491067d8c07496c8afe8
pxndxt <hebertlinaressipe1996@gmail.com>
2022-08-16 07:37:36 +0800

    hola mundo

--- config/db.js
+++ config/db.js
@@ -1,29 +1,25 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
-const DB_URI = `mongodb://localhost:27017/my_app_node`
+const DB_URI = `mongodb+srv://hebert:hebert@cluster0.kj6gc.mongodb.net/my_app_node?retryWrites=true&w=majority`;
 
 module.exports = () => {
+	const connect = () => {
+		mongoose.connect(
+			DB_URI,
+			{
+				keepAlive: true,
+				useNewUrlParser: true,
+				useUnifiedTopology: true,
+			},
+			(err) => {
+				if (err) {
+					console.log('DB: ERROR !!');
+				} else {
+					console.log('Conexion correcta!!');
+				}
+			}
+		);
+	};
 
-    const connect = () => {
-
-        mongoose.connect(
-            DB_URI,
-            {
-                keepAlive: true,
-                useNewUrlParser: true,
-                useUnifiedTopology: true
-            },
-            (err) => {
-                if (err) {
-                    console.log('DB: ERROR !!');
-                } else {
-                    console.log('Conexion correcta!!')
-                }
-            }
-        )
-
-    }
-
-    connect();
-
-}
+	connect();
+};
